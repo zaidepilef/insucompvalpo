2000b45caedbfdb0744117f058af07cc4285b9ca
Felipe Diaz <fel.di.rod@gmail.com>
2021-05-13 10:06:52 -0400

    .,.

---
+++ test/precache-manifest.f5481030a7e4f3c3c9fe0769d619f2f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623175f31fdf00ab616b3b8a1a06a7c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d49c3179e7b848e880",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d77468be3ce107e8bac",
+    "url": "/static/js/2.1427a7a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1427a7a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d49c3179e7b848e880",
+    "url": "/static/js/main.34cf3a9a.chunk.js"
+  },
+  {
+    "revision": "bfd64496c9f167277093",
+    "url": "/static/js/runtime-main.64dfed54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
